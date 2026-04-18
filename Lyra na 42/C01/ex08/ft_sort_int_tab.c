8599f1f19682aaeb1c89826b3819dedadd67bc96
Lucia Katze Lyra Lino De Assis <llyra-li@c3r5p4.42sp.org.br>
2026-04-18 19:10:07 -0300

    New files, updated ones, commented versions comming soon.

---
+++ Lyra na 42/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llyra-li <llyra-li@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/09 10:30:22 by llyra-li          #+#    #+#             */
+/*   Updated: 2026/04/09 11:01:28 by llyra-li         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	i;
+	int	order;
+
+	order = 0;
+	i = 0;
+	while (order == 0)
+	{
+		order = 1;
+		while (i < (size - 1))
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+				order = 0;
+			}
+		i++;
+		}
+		i = 0;
+	}
+}
