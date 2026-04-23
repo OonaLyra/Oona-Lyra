bdbc5c5fee8d2d3956c782150023ab4f24e3fcd7
Lucia Katze Lyra Lino De Assis <llyra-li@c3r5p3.42sp.org.br>
2026-04-23 13:30:09 -0300

    lil updates

---
+++ Lyra na 42/C06/ex01/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llyra-li <llyra-li@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 12:51:53 by llyra-li          #+#    #+#             */
+/*   Updated: 2026/04/23 12:57:05 by llyra-li         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	t;
+
+	t = 0;
+	while (str[t] != 0)
+	{
+		write(1, &str[t], 1);
+		t++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i <= (argc - 1))
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Oona-Lyra
+
+ Esse repositório armazena os exercícios e projetos dos quais eu fiz parte durante a piscina da 42SP
